88964460c70196b3e2cbb01a2447285f9b8680a3
GregTheBingoGuy <gregory@santafe.co.cr>
2020-07-28 00:15:32 -0600

    Agregar referencias a doc.
    Agregar referencias a doc.

--- PruebasBackEnd/TestUsuarioDAL.cs
+++ PruebasBackEnd/TestUsuarioDAL.cs
@@ -5,6 +5,11 @@ using BackEnd.Entities;
 using BackEnd.Libraries;
 
 
+//https://medium.com/@mehanix/lets-talk-security-salted-password-hashing-in-c-5460be5c3aae
+//https://stackoverflow.com/questions/2138429/hash-and-salt-passwords-in-c-sharp
+//https://docs.microsoft.com/en-us/aspnet/core/security/data-protection/consumer-apis/password-hashing?view=aspnetcore-3.1
+//https://forums.asp.net/t/2155576.aspx?how+to+hash+salt+password+in+Register+and+Login
+//https://blog.securityinnovation.com/blog/2011/03/how-to-hash-and-salt-passwords-in-aspnet.html
 
 namespace PruebasBackEnd
 {
